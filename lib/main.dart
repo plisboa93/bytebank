002c3f570daacdacbb79e59503cb586ac8a024bf
PedroG <pedrogabriel3@gmail.com>
2020-04-01 04:48:38 -0300

    add os itens transf

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,48 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-      home: Scaffold(
-        body: Column(
-          children: <Widget>[
-            Card(
-              child: ListTile(
-                leading: Icon(Icons.monetization_on),
-                title: Text('100.0'),
-                subtitle: Text('1000'),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                leading: Icon(Icons.monetization_on),
-                title: Text('400.0'),
-                subtitle: Text('7000'),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                leading: Icon(Icons.monetization_on),
-                title: Text('800.0'),
-                subtitle: Text('15000'),
-              ),
-            ),
-            Card(
-              child: ListTile(
-                leading: Icon(Icons.monetization_on),
-                title: Text('900.0'),
-                subtitle: Text('11000'),
-              ),
-            ),
-          ],
-        ),
-        appBar: AppBar(
-          title: Text('Transferências'),
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-        ),
-      ),
-    ));
+  home: Scaffold(
+    body: ListaTransferencias(),
+    appBar: AppBar(
+      title: Text('Transferências'),
+    ),
+    floatingActionButton: FloatingActionButton(
+      child: Icon(Icons.add),
+    ),
+  ),
+));
+
+class ListaTransferencias extends StatelessWidget {                      
+  @override 
+  Widget build(BuildContext context){
+
+    return Column(
+      children: <Widget>[
+        ItemTransferencia(),
+        ItemTransferencia(),
+        ItemTransferencia(),
+      ],
+    );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+
+  final String valor;
+  final String numeroConta;
+
+  ItemTransferencia(this.valor, this.numeroConta);
+
+  
+
+ @override 
+ Widget build(BuildContext context){
+   return Card(
+     child: ListTile(
+       leading: Icon(Icons.monetization_on),
+       title: Text('5000.0'),
+       subtitle: Text('16000'),
+     )
+   );
+ }
+}
